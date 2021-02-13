0ece0d7bcd24fd7f7d6d480dd28b7a2db2d41c1d
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-02-14 02:01:35 +0530

    Added firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,19 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
+import config from './data.json';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBAUs20gLeSPlEeCM0etyhh-urZj9wCQQs",
-  authDomain: "react-slack-clone-6096b.firebaseapp.com",
-  projectId: "react-slack-clone-6096b",
-  storageBucket: "react-slack-clone-6096b.appspot.com",
-  messagingSenderId: "112987856959",
-  appId: "1:112987856959:web:1d8b6711c432a4094b188a",
-  measurementId: "G-4TNN05YPN1"
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
-
 
 export default firebase;
