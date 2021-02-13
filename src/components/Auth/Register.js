a2181c75fc38ac4781579fa6e31c2e62344171f9
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-02-14 01:52:46 +0530

    Register user with email
    Register user with email & password using firebase authentication.

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import firebase from "../../firebase";
 
 const Register = () => {
 
@@ -11,16 +12,29 @@ const Register = () => {
 
   const { email, password, password2 } = formData;
 
+  const onSubmit = async () => {
+    try {
+      let user = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      console.log(user);
+      setFormData({email: '', password: '', password2: ''});
+    } catch (err) {
+      console.error(err);
+    }
+    
+  }
+
   return (
     <Fragment>
       <div className="container text-center">
         <div className="row">
-          <h3 className="blue-text darken-1"><i className="material-icons large">chat</i><br /> Chat App</h3>
+          <h4 className="blue-text darken-4">
+            <i className="material-icons large">chat</i><br />Register for Chat App
+          </h4>
         </div>
         <div className="row">
           <div className="col s3 m2"></div>
           <div className="col s6 m8 card register">            
-            <h4>Register</h4>
+            {/* <h4>Register</h4> */}
             <div className="row">
               <div className="input-field col s12">
                 <i className="material-icons prefix">email</i>
@@ -46,7 +60,7 @@ const Register = () => {
               </div>
             </div>
             <div className="row">
-              <a className="waves-effect waves-light btn">
+              <a className="waves-effect waves-light btn" onClick={onSubmit}>
                 <i className="material-icons right">send</i>
                 Submit
               </a>
